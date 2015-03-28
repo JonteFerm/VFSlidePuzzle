029efb769d66e132ccde64b3c983aa53a960230a
ErikVincelette <multisol5@hotmail.com>
2015-03-28 13:00:33 +0100

    Adds brickAdapter test class

---
+++ app/src/androidTest/java/com/example/vf/vfslidepuzzle/BrickAdapterTests.java
@@ -0,0 +1,16 @@
+package com.example.vf.vfslidepuzzle;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+/**
+ * Created by Erik on 2015-03-28.
+ */
+public class BrickAdapterTests {
+    @Test
+    void checkMove(){
+        Assert.assertEquals(false, true);
+    }
+}
